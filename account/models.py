ac70c1495440480427ff7f73967dc94d6ca74162
nildiert <niljordan23@gmail.com>
2020-10-01 12:36:57 -0500

    [FEATURE] Added bookmarklet launcher js

--- account/models.py
+++ account/models.py
@@ -11,6 +11,7 @@ class Profile(models.Model):
     date_of_birth = models.DateField(blank=True, null=True)
     photo = models.ImageField(upload_to='users/%Y/%m/%d',
                               blank=True)
+    
 
     def __str__(self):
         return f'Profile for user {self.user.username}'
